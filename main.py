5c1e1d9b17c4c23b96ca59aebd72488543898e5c
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-09 19:17:04 +0100

    Add QC metrics calculation and saving functionality

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from config import AB1_FOLDER
 
+from scripts.preprocessing import *
+
+abi_records = load_ab1_files(AB1_FOLDER)
+
+processed_reads = prepare_reads(abi_records)
+
+qc_summary = calculate_qc_metrics(processed_reads)
+
+save_qc_summary(qc_summary)
